84c603c4f041d36d8a27517c446f134dcafaa10d
Hoàng Ngựa <dinhhuyhoangcb02@gmail.com>
2026-06-01 14:38:54 +0700

    Implement evaluate_board function
    Add board evaluation function for chess game.

--- src/engine/evaluation.py
+++ src/engine/evaluation.py
@@ -1,1 +1,26 @@
 # Ham luong gia ban co
+import chess
+from .constants import PIECE_VALUES, PST
+
+def evaluate_board(board):
+    if board.is_checkmate():
+        return -99999 if board.turn == chess.WHITE else 99999
+    if board.is_stalemate() or board.is_insufficient_material():
+        return 0
+
+    score = 0
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if not piece:
+            continue
+            
+        val = PIECE_VALUES[piece.piece_type]
+        
+        if piece.color == chess.WHITE:
+            pst_val = PST[piece.piece_type][square]
+            score += (val + pst_val)
+        else:
+            pst_val = PST[piece.piece_type][chess.square_mirror(square)]
+            score -= (val + pst_val)
+            
+    return score
